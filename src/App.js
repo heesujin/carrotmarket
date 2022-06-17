1c7d45b6b3d32cb83ce5c7a12d1ba02c33e06ba4
heesujin <hs970502@naver.com>
2022-06-17 13:51:01 +0900

    시작 페이지 html css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Start from "./page/Start";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Start />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #212121;
+  background-size: cover;
+`;
 
 export default App;
---
+++ src/page/Start.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import carrot from "../image/당근마켓.png";
+
+function Start() {
+  return (
+    <All>
+      <div>
+        <img src={carrot} alt="" />
+      </div>
+      <Comments>
+        <Title>당신 근처의 당근마켓</Title>
+        <Pone>우리동네 중고거래를 편리하게! </Pone>
+        <Ptwo>가입하고 중고서비스를 이용하세요!</Ptwo>
+      </Comments>
+      <SignUpBtn>회원가입</SignUpBtn>
+
+      <Login>
+        이미 계정이 있나요? <LoginBtn>로그인</LoginBtn>
+      </Login>
+    </All>
+  );
+}
+
+const All = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Comments = styled.div`
+  margin-top: 50px;
+`;
+
+const Title = styled.h3`
+  color: white;
+`;
+
+const Pone = styled.p`
+  color: white;
+  margin-left: -20px;
+`;
+const Ptwo = styled.p`
+  color: white;
+  margin-left: -30px;
+`;
+
+const SignUpBtn = styled.button`
+  color: white;
+  background-color: #ee8548;
+  border: none;
+  padding: 18px;
+  width: 500px;
+  margin-left: -150px;
+  margin-top: 50px;
+  border-radius: 10px;
+  font-size: large;
+`;
+
+const Login = styled.p`
+  margin-top: 30px;
+  color: white;
+`;
+
+const LoginBtn = styled.span`
+  color: #ee8548;
+`;
+
+export default Start;
