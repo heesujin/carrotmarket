7c0daec4be8bfcdf5eaace7c7e4a7bfbc6e805ed
LeeHuynSuk <gusehf321@naver.com>
2022-06-17 18:00:01 +0900

    확인

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import "./App.css";
 //페이지 가져오기
-<<<<<<< Updated upstream
-=======
-import Start from "./page/Start";
-import Login from "./page/Login";
-import SingUp from "./page/Singup";
-import Main from "./page/Main";
-import Detail from "./page/Detail";
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 import styled from "styled-components";
 import Router from "./Router";
 
 function App() {
   return (
     <Container>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
       <Router />
-=======
-      <Detail />
->>>>>>> Stashed changes
-=======
-      <Detail />
->>>>>>> Stashed changes
     </Container>
   );
 }
